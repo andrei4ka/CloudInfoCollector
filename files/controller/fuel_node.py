5ff87c70390a87e1d78637e19f8815181563cf9c
andrei4ka <akirilochkin@mirantis.com>
2014-03-12 17:12:27 +0400

    bugfix controller_dir

--- files/controller/fuel_node.py
+++ files/controller/fuel_node.py
@@ -1,11 +1,11 @@
-controller_dir = target_dir + '/fuel-master/'
+master_dir = target_dir + '/fuel-master/'
 try:
-  os.mkdir(controller_dir)
+  os.mkdir(master_dir)
 except:
   pass
 
 os.system('fuel nodes > /etc/fuel-nodes')
 files = ['/var/log/', '/etc/my.cnf', '/etc/mysql/', '/etc/fuel-nodes', '/etc/fuel-uuid', '/etc/naily.facts', '/etc/nailgun/', '/etc/naily/', '/etc/haproxy/']
 for file in files:
-  os.system('cp -a ' + file + ' ' + controller_dir)
+  os.system('cp -a ' + file + ' ' + master_dir)
 
